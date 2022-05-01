a049dee389a8d38f623c748813bf064146c4cd2d
cdpjenkins <cdpjenkins@gmail.com>
2022-05-01 23:47:19 +0100

    UART write string

--- BareMetal/boot/main.s
+++ BareMetal/boot/main.s
@@ -1,11 +1,18 @@
+.data
+message: .asciz "Hello raspberry pi"
+
 .text
 .global main
 
 main:
     stp x29, x30, [sp, #-16]!
 
+    bl init_uart
+
+    adr x0, message
+    bl write_string
+    
     mov x0, #0
 
     ldp x29, x30, [sp], #16
     ret
-    
