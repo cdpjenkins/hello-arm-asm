16148e76d79e2026785fc62f9f7e6dc5e94fb705
cdpjenkins <cdpjenkins@gmail.com>
2022-05-02 08:39:58 +0100

    Turn main into C

---
+++ BareMetal/main.c
@@ -0,0 +1,18 @@
+int init_uart();
+int write_string(char *str);
+int read_char();
+void write_char(int ch);
+
+int main() {
+
+    init_uart();
+
+    write_string("Hello from some C code!!!1\n");
+
+    for (;;) {
+        char ch;
+
+        ch = read_char();
+        write_char(ch);
+    }
+}
