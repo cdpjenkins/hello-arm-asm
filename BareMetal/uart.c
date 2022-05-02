6a0cb27178a424dab00890384bc0e4a87aca8826
cdpjenkins <cdpjenkins@gmail.com>
2022-05-02 22:42:25 +0100

    Tidy up

--- BareMetal/uart.c
+++ BareMetal/uart.c
@@ -1,24 +1,3 @@
-
-// TODO turn all of this into a big struct
-#define IO_BASE_ADDR    0x3f200000
-#define UART0_DR        ((volatile unsigned int *) (IO_BASE_ADDR + 0x1000))
-#define UART0_FR        ((volatile unsigned int *) (IO_BASE_ADDR + 0x1018))
-#define UART0_IBRD      ((volatile unsigned int *) (IO_BASE_ADDR + 0x1024))
-#define UART0_FBRD      ((volatile unsigned int *) (IO_BASE_ADDR + 0x1028))
-#define UART0_LCRH      ((volatile unsigned int *) (IO_BASE_ADDR + 0x102c))
-#define UART0_CR        ((volatile unsigned int *) (IO_BASE_ADDR + 0x1030))
-#define UART0_IMSC      ((volatile unsigned int *) (IO_BASE_ADDR + 0x1038))
-
-#define RXFE            (1 << 4)
-#define TXFF            (1 << 5)
-
-#define FEN             (1 << 4)
-#define WLEN_8_BITS     (0b11 << 6)
-
-#define UARTEN          (1 << 0)
-#define TXE             (1 << 8)
-#define RXE             (1 << 9)
-
 typedef struct {
     volatile unsigned int dr;
     volatile unsigned int rsrecr;
@@ -38,6 +17,19 @@ typedef struct {
     // there are loads more regs that we don't use
 } UART_Registers;
 
+#define bit(n)          (1 << n)
+
+#define RXFE            bit(4)
+#define TXFF            bit(5)
+
+#define FEN             bit(4)
+#define WLEN_8_BITS     (0b11 << 6)
+
+#define UARTEN          bit(0)
+#define TXE             bit(8)
+#define RXE             bit(9)
+
+#define IO_BASE_ADDR    0x3f200000
 #define UART0 ((UART_Registers *) (IO_BASE_ADDR + 0x1000))
 
 void uart_init() {
@@ -53,17 +45,14 @@ void uart_init() {
 }
 
 int uart_read_char() {
-    while (UART0->fr & RXFE) {
-        // wait until we actually have a char to read
-    }
+    while (UART0->fr & RXFE) { } // wait until we actually have a char to read
 
-    return *UART0_DR;
+
+    return UART0->dr;
 }
 
 void uart_write_char(unsigned int ch) {
-    while (UART0->fr & TXFF) {
-        // wait until there is room in the transmit FIFO for our char
-    }
+    while (UART0->fr & TXFF) { } // wait until there is room in the transmit FIFO for our char
 
     UART0->dr = ch;
 }
