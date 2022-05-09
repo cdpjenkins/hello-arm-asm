63848f2be6ac638f0ea905ecb90f126c780aa925
cdpjenkins <cdpjenkins@gmail.com>
2022-05-09 19:50:26 +0100

    Add printk_uint32_hex to print hex values

--- BareMetal/main.c
+++ BareMetal/main.c
@@ -6,9 +6,16 @@ int read_char();
 void write_char(int ch);
 
 int main() {
+    int i;
+
     uart_init();
     printk_string("Hello from some C code!!!1\n\n\n");
     printk_string("LOL here is another string\n");
+
+    for (i = 0; i < 4096; i++) {
+        printk_uint32_hex(i);
+        printk_string("\r\n");
+    }
 
     for (;;) {
         char ch;
--- BareMetal/printk.h
+++ BareMetal/printk.h
@@ -1,8 +1,11 @@
 #ifndef _PRINTK_H
 #define _PRINTK_H
 
+#include "stdint.h"
+
 #include "uart.h"
 
 void printk_string(const char *str);
+void printk_uint32_hex(uint32_t x);
 
 #endif // _PRINTK_H
