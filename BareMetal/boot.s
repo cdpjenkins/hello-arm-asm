e41593733e00321ad5f082f8216d0c90eed7fe84
cdpjenkins <cdpjenkins@gmail.com>
2022-05-10 10:20:13 +0100

    Switch to EL1 before starting main

--- BareMetal/boot.s
+++ BareMetal/boot.s
@@ -10,8 +10,25 @@ end:
     b end
 
 kernel_entry:
+    // switch to EL1
+    mrs x0, currentel
+    lsr x0, x0, #2
+    cmp x0, #2
+    bne end                 // if we're not in EL2 then that's an error and we terminate
+    msr sctlr_el1, xzr
+    mov x0, #(1 << 31)
+    msr hcr_el2, x0
+    mov x0, #0b1111000101
+    msr spsr_el2, x0
+    adr x0, el1_entry
+    msr elr_el2, x0
+    eret
+el1_entry:
+
+    // setup initial stack pointer
     mov sp, #0x80000
 
+    // zero the whole BSS
     ldr x0, =bss_start
     ldr x1, #0
     ldr x2, =bss_end
